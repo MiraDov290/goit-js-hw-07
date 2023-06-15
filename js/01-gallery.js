81cb641896ad7b716489cc1ad5aac03ce6cb0852
MiraDov290 <muzhyk.myroslava@gmail.com>
2023-06-15 21:18:00 +0100

    changers file

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,14 +1,16 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+
 const galleryContainer = document.querySelector('.gallery');
-const itemsMarkup = createGalleryItemsMarkup('galleryItems');
+const itemsMarkup = createGalleryItemsMarkup(galleryItems);
 galleryContainer.insertAdjacentHTML('beforeend', itemsMarkup);
 galleryContainer.addEventListener('click', onImgClick);
 
+
 function createGalleryItemsMarkup(items) {
-    return items
-        .map(({ preview, original, description }) => {
+  return items
+    .map(({ preview, original, description }) => {
       return `<li class="gallery__item">
   <a class="gallery__link" href="${original}">
     <img
@@ -19,9 +21,40 @@ function createGalleryItemsMarkup(items) {
     />
   </a>
 </li>`;
-        })
-        .join(' ');  
+    })
+    .join('');
+}
+
+// console.log(galleryItems);
+
+
+
+
+const instance = basicLightbox.create(`
+<img width="1280" height="auto" src="">`,{
+    onShow: (instance) => {
+      window.addEventListener('keydown', onEscKeyPress);
+    },
+    onClose: (instance) => {
+      window.removeEventListener('keydown', onEscKeyPress);
+    },
+  }
+);
+
+function onImgClick(event) {
+  event.preventDefault();
+  const datasetSource = event.target.dataset.source;
+  if (!datasetSource) return;
+  instance.element().querySelector('img').src = datasetSource;
+  instance.show();
+}
+
+function onEscKeyPress(event) {
+  if (event.code !== 'Escape') return;
+  instance.close();
 }
 
 
-//console.log(galleryItems);
+
+
+
